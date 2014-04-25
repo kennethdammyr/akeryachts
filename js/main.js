d946e66d63defdaef6a517ff01ad79dcbb469a7c
kennethdammyr <kennethdammyr@gmail.com>
2014-04-25 16:16:58 +0200

    Fredaginnsats
    Eksperimentert med adaptive-images, jobbet med layout og quotes.

--- js/main.js
+++ js/main.js
@@ -1,59 +1,67 @@
-$(window).load(function () {
-
-    "use strict";
-
-    // Remove loader
-
-    $('#progress-bar').width('100%');
-    $('#loader').hide();
-
-});
-
-var loader = function(){
-	this.count	= 1,
-	this.n		= $( 'img' ).length,
-	this.dwidth = $(window).width() / this.n,
-	this.show	= function(){
-		console.log("Vi har dwidth: ", this.dwidth);	
-		
-		
-		$('img').load(function () {
-
-        $('#progress-bar').css('width', this.count * this.dwidth);
-		//$('#progress-bar').width( count * bredde, "%");
-        this.count = this.count + 1;
-		console.log("jobber...", this.count);
-    });
-		
-	},
-	this.remove	= function(){
-		console.log("Takk for seg")	
-	}
-}
-
-
 $(document).ready(function () {
-
     "use strict";
 	
+	var navbarHeight = $("#ay-navbar").height();
+	var contentHeight = $(window).height() - navbarHeight;
+	$("#content").css("min-height",contentHeight);
+	
+	
+	
+
+	
 	$(".navbar-nav > li, .navbar-brand").click(function(){
+		
 		var status = "";
-		var page = $(this).prop('id');
+		var page = $(this).prop('id'); // Hvor skal vi
+		
 		$("#content").load(page + ".html", function(response, status, xhr){
+			
 			if(status == "error"){
 				$("#content").html("<div class='row firstrow'><div class='col-xs-10 col-xs-offset-1'><h3>Vi er veldig lei oss, men her er det noe galt</h3></div></div><div id='push'></div>");
 			}
+			initQuoteLoop();
 		});
+
 	});
 
-	/*
+	
 	$("#content").load("main.html", function(){
 		console.log("Henta main");
-
+		initQuoteLoop();
 	});
+	
+	
 	$("#footer").load("footer.html", function(){
 		console.log("henta footer");	
 	});
-	*/
-    
+	
+	function apiCall(done){
+		// API-Kall henter data
+		// Må kunne sende random-data til quote
+		// Må kunne plukke spesifikk data til sider osv.
+		done("Aker yachts er det beste firmaet jeg har brukt noen gang!");
+	}
+	
+	function initQuoteLoop(){
+		console.log("Starting Quote-loop on all blockquote-tags");	
+		
+		$("blockquote").each(function(index){
+			console.log("Found Blockquote with id:", index);
+			var blockquote = this;
+			changeQuote(this);
+			setInterval(function(){changeQuote(blockquote);},25000);
+		});
+	}
+	
+	function changeQuote(blockquote) {
+		console.log("Changing blockquote...");
+		apiCall(function(data){		
+			$(blockquote).fadeOut('slow', function(){
+				$(blockquote).html("<p>"+data+"</p><footer>Lars</footer>");	
+				$(blockquote).fadeIn('slow');
+			});		
+		});
+	}
+
+   
 });
