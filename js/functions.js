21b63a1dd9fecf23ed22fda466dfac99e04edae9
Kenneth Dammyr <kennethdammyr@gmail.com>
2014-10-06 21:43:09 +0200

    Nermer seg

--- js/functions.js
+++ js/functions.js
@@ -229,7 +229,7 @@ function createContact(){
     console.log("Vi kjører kontaktgenerering");
     apiCall('kontakt', false, 0, function(kontakt){
         console.log(kontakt.besoksadresse);
-        $('#visit_adress').html("<h3>Besøksadresse</h3>", kontakt.besoksadresse);
+        $('#visit_adress').append("<h3>Besøksadresse</h3>", kontakt.besoksadresse);
         $('#postal_adress').append("<h3>Postadresse</h3>", kontakt.postadresse);
         
     });
